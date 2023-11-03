1b9b07f828c4c8c4fa12cd3c02fc4fbb876fd6eb
ychilo <seojunchian@gmail.com>
2023-11-03 16:46:27 +0300

    a

--- public/javascript/faucet.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("home").addEventListener("click", () => {
-    window.location.href = "/";
-});
-document.getElementById("coffee").addEventListener("click", () => {
-    window.location.href = "coffee";
-});
--- public/javascript/home.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("faucet").addEventListener("click", () => {
-    window.location.href = "faucet";
-});
-document.getElementById("coffee").addEventListener("click", () => {
-    window.location.href = "coffee";
-});
