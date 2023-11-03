8b3d22b42b0eeb83b8f4c1010b084e3b80fcb6dd
ychilo <seojunchian@gmail.com>
2023-11-03 18:28:17 +0300

    token file created

---
+++ contracts/LogitechToken.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LogitechToken is ERC20("Logitech Token", "LOG") {
+}
