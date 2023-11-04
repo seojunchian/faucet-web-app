8b95bfd7f11977bea1f51c9dfd20fae3be024f5e
ychilo <seojunchian@gmail.com>
2023-11-04 10:30:03 +0300

    rest of the change

--- app.js
+++ app.js
@@ -16,10 +16,9 @@ app.use(express.static(__dirname + "/public"));
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
-    if (!error)
-        console.log(
-            "Server is Successfully Running, and App is listening on port " +
-                PORT
-        );
-    else console.log("Error occurred, server can't start", error);
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
 });
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    sepolia: {
+    ganache: {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
