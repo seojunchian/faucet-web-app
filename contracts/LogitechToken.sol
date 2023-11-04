eb3f4e6ed3c32a46ae3290bc2393f83be07af9b5
ychilo <seojunchian@gmail.com>
2023-11-04 16:29:54 +0300

    kind of erc20 contract complete

--- contracts/LogitechToken.sol
+++ contracts/LogitechToken.sol
@@ -7,20 +7,10 @@ contract LogitechToken {
   function name() public view returns (string memory) {
     return _name;
   }
-  
+
   string private _symbol;
   function symbol() public view returns (string memory) {
     return _symbol;
-  }
-
-  constructor(string memory name_, string memory symbol_) {
-    _name = name_;
-    _symbol = symbol_;
-  }
-  
-  mapping(address => uint256) private _balances;
-  function balanceOf(address _account) public view returns (uint256) {
-    return _balances[_account];
   }
 
   uint256 private _totalSupply;
@@ -28,9 +18,48 @@ contract LogitechToken {
     return _totalSupply;
   }
 
-  function transfer(address owner, address to, uint256 value) public view {
-    value -= _balances[owner];
-    value += _balances[to];
+  mapping(address => uint256) private _balances;
+  function balanceOf(address _account) public view returns (uint256) {
+    return _balances[_account];
   }
 
+  constructor(string memory name_, string memory symbol_, uint256 totalSupply_) {
+    _name = name_;
+    _symbol = symbol_;
+    _totalSupply = totalSupply_;
+    _balances[msg.sender] += totalSupply_;
+  }
+
+  mapping(address => mapping (address => uint256)) allowed;
+
+  event Transfer(address sender, address receiver, uint256 numTokens);
+  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+  function transfer(address receiver, uint256 numToken) public returns (bool) {
+    require(numToken <= _balances[msg.sender], "Cant send more than the total supply");
+    _balances[msg.sender] -= numToken;
+    _balances[receiver] += numToken;
+    emit Transfer(msg.sender, receiver, numToken);
+    return true;
+  }
+
+  function approve(address delegate, uint256 numTokens) public returns (bool) {
+    allowed[msg.sender][delegate] = numTokens;
+    emit Approval(msg.sender, delegate, numTokens);
+    return true;
+  }
+
+  function allowance(address owner, address delegate) public view returns (uint256) {
+    return allowed[owner][delegate];
+  }
+
+  function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+    require(numTokens <= _balances[owner]);
+    require(numTokens <= allowed[owner][msg.sender]);
+    _balances[owner] = _balances[owner] - numTokens;
+    allowed[owner][msg.sender] = allowed[buyer][msg.sender] - numTokens;
+    _balances[buyer] = _balances[buyer] + numTokens;
+    emit Transfer(owner, buyer, numTokens);
+    return true;
+  }
 }
