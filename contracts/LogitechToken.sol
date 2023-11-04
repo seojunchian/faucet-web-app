7230ea4c1b9b859ef4871d1c9c2e37643dcda6ff
ychilo <seojunchian@gmail.com>
2023-11-04 11:01:00 +0300

    setting up erc20 token

--- contracts/LogitechToken.sol
+++ contracts/LogitechToken.sol
@@ -3,16 +3,6 @@ pragma solidity ^0.8.19;
 
 contract LogitechToken {
 
-  mapping(address => uint256) _balances;
-  function balanceOf(address _account) public view returns (uint256) {
-    return _balances[_account];
-  }
-
-  uint256 private _totalSupply;
-  function totalSupply() public view returns (uint256) {
-    return _totalSupply;
-  }
-
   string private _name;
   function name() public view returns (string memory) {
     return _name;
@@ -26,6 +16,16 @@ contract LogitechToken {
   constructor(string memory name_, string memory symbol_) {
     _name = name_;
     _symbol = symbol_;
+  }
+  
+  mapping(address => uint256) private _balances;
+  function balanceOf(address _account) public view returns (uint256) {
+    return _balances[_account];
+  }
+
+  uint256 private _totalSupply;
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
   }
 
   function transfer(address owner, address to, uint256 value) public view {
