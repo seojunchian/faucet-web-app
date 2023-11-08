614681ad47f3fcbad4a5b23b5da3d5e0403a8972
ychilo <seojunchian@gmail.com>
2023-11-08 13:45:12 +0300

    coffee.js fixed now can access to metamask

--- public/javascript/coffee.js
+++ public/javascript/coffee.js
@@ -5,13 +5,21 @@ document.getElementById("faucet").addEventListener("click", () => {
   window.location.href = "faucet";
 });
 document.getElementById("log-in").addEventListener("click", async () => {
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  const balances = await window.ethereum.request({
-    method: "eth_getBalance",
-    params: [accounts[0]],
-  });
-  document.getElementById("address").innerHTML = "Address: " + accounts[0];
-  document.getElementById("balance").innerHTML = "Balance: " + balances;
+  let address = document.getElementById("address");
+  let balance = document.getElementById("balance");
+  let provider = window.ethereum;
+  if (typeof provider !== "undefined") {
+    // metamask installed.
+    provider
+      .request({method: "eth_requestAccounts"})
+      .then((accounts) => {
+        address.innerHTML = "Address: " + accounts[0];
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    provider.on("accountsChanged", (accounts) => {
+      address.innerHTML = "Address: " + accounts[0];
+    });
+  }
 });
