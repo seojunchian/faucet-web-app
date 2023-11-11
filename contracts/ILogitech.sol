9d05552dba78acac791efa1fb27929e51b374c64
ychilo <seojunchian@gmail.com>
2023-11-11 18:31:52 +0300

    contracts for deployment completed to run

--- contracts/ILogitech.sol
+++ contracts/ILogitech.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.19;
 
 interface ILogitech {
-  error InvalidSender(address sender);
-  error InvalidReceiver(address receiver);
-  error InsufficientBalance(address sender, uint256 balance, uint256 needed);
-  error InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
-  error InvalidApprover(address approver);
-  error InvalidSpender(address spender);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+	error InvalidSender(address sender);
+	error InvalidReceiver(address receiver);
+	error InsufficientBalance(address sender, uint256 balance, uint256 needed);
+	error InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
+	error InvalidApprover(address approver);
+	error InvalidSpender(address spender);
+	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(address indexed owner, address indexed spender, uint256 amount);
 }
--- contracts/Logitech.sol
+++ contracts/Logitech.sol
@@ -4,71 +4,80 @@ pragma solidity ^0.8.19;
 import {ILogitech} from "./ILogitech.sol";
 
 contract Logitech is ILogitech {
-  mapping(address account => uint256) private _balances;
-  uint256 private _totalSupply;
-  string private _name;
-  string private _symbol;
+	mapping(address => uint256) private balances;
 
-  constructor(string memory name_, string memory symbol_) {
-    _name = name_;
-    _symbol = symbol_;
-    mint(msg.sender, 10000000000000000000000000);
-  }
+	uint256 private _totalSupply;
 
-  function name() public view virtual returns (string memory) {
-    return _name;
-  }
+	string private _name;
+	string private _symbol;
 
-  function symbol() public view virtual returns (string memory) {
-    return _symbol;
-  }
+	constructor(string memory name_, string memory symbol_) {
+		_name = name_;
+		_symbol = symbol_;
+		mint(msg.sender, 1);
+	}
 
-  function totalSupply() public view virtual returns (uint256) {
-    return _totalSupply;
-  }
+	function name() public view virtual returns (string memory) {
+		return _name;
+	}
 
-  function balanceOf(address account) public view virtual returns (uint256) {
-    return _balances[account];
-  }
+	function symbol() public view virtual returns (string memory) {
+		return _symbol;
+	}
 
-  function transferFrom(address from, address to, uint256 amount) internal {
-    if(from == address(0)) {
-      revert InvalidSender(from);
-    }
-    if(to == address(0)) {
-      revert InvalidReceiver(to);
-    }
-    update(from, to, amount);
-  }
+	function totalSupply() public view virtual returns (uint256) {
+		return _totalSupply;
+	}
 
-  function update(address from, address to, uint256 value) internal virtual {
-    if (from == address(0)) {
-      _totalSupply += value;
-    } else {
-      if (_balances[from] < value) {
-        revert InsufficientBalance(from, _balances[from], value);
-      }
-      unchecked {
-        _balances[from] -= value;
-      }
-    }
+	function balanceOf(address account) public view virtual returns (uint256) {
+		return balances[account];
+	}
 
-    if (to == address(0)) {
-      unchecked {
-        _totalSupply -= value;
-      }
-    } else {
-      unchecked {
-        _balances[to] += value;
-      }
-    }
-    emit Transfer(from, to, value);
-  }
+	function transferFrom(address from, address to, uint256 amount) internal {
+		if(from == address(0)) {
+			revert InvalidSender(from);
+		}
+		if(to == address(0)) {
+			revert InvalidReceiver(to);
+		}
+		update(from, to, amount);
+	}
 
-  function mint(address to, uint256 amount) internal {
-    if (to == address(0)) {
-      revert InvalidReceiver(address(0));
-    }
-    update(address(0), to, amount);
-  }
+	function update(address from, address to, uint256 value) internal virtual {
+		if (from == address(0)) {
+			_totalSupply += value;
+		} else {
+			if (balances[from] < value) {
+				revert InsufficientBalance(from, balances[from], value);
+			}
+			unchecked {
+				balances[from] -= value;
+			}
+		}
+
+		if (to == address(0)) {
+			unchecked {
+				_totalSupply -= value;
+			}
+		} else {
+			unchecked {
+				balances[to] += value;
+			}
+		}
+		emit Transfer(from, to, value);
+	}
+
+	function mint(address to, uint256 amount) internal {
+		if (to == address(0)) {
+			revert InvalidReceiver(address(0));
+		}
+		update(address(0), to, amount);
+	}
+
+	function burn(address from, uint256 amount) internal {
+		if (from == address(0)) {
+			revert InvalidSender(from);
+		}
+		update(from, address(0), amount);
+	}
 }
